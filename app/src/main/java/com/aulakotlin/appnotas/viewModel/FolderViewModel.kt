c84cce923cba6e97dc8cbe0891824c4e44b9e1b0
EnzoDelmachio <enzo.dlmc@gmail.com>
2025-04-08 17:07:28 -0300

    :sparkles: feat: view models

---
+++ app/src/main/java/com/aulakotlin/appnotas/viewModel/FolderViewModel.kt
@@ -0,0 +1,21 @@
+package com.aulakotlin.appnotas.viewModel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.aulakotlin.appnotas.data.db.AppDatabase
+import com.aulakotlin.appnotas.data.model.Folder
+import kotlinx.coroutines.launch
+
+class FolderViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val db = AppDatabase.getInstance(application)
+    val folders: LiveData<List<Folder>> = db.folderDao().getAll()
+
+    fun insert(folder: Folder) {
+        viewModelScope.launch {
+            db.folderDao().insert(folder)
+        }
+    }
+}
---
+++ app/src/main/java/com/aulakotlin/appnotas/viewModel/NoteViewModel.kt
@@ -0,0 +1,24 @@
+package com.aulakotlin.appnotas.viewModel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.aulakotlin.appnotas.data.db.AppDatabase
+import com.aulakotlin.appnotas.data.model.Note
+import kotlinx.coroutines.launch
+
+class NoteViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val db = AppDatabase.getInstance(application)
+
+    fun getNotes(folderId: Long) = db.noteDao().getNotesByFolderId(folderId)
+
+    fun insert(note: Note) = viewModelScope.launch {
+        db.noteDao().insert(note)
+    }
+
+    fun update(note: Note) = viewModelScope.launch {
+        db.noteDao().update(note)
+    }
+
+}
