0bd14e8b98824f65fb9b6e487641981bd7d105f3
EnzoDelmachio <enzo.dlmc@gmail.com>
2025-04-14 17:26:29 -0300

    :recycle: refactor: refactoring base entities

--- app/src/main/java/com/aulakotlin/appnotas/data/model/Folder.kt
+++ app/src/main/java/com/aulakotlin/appnotas/data/model/Folder.kt
@@ -5,6 +5,6 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "folders")
 data class Folder (
-    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    @PrimaryKey(autoGenerate = true) val id: Long = 0,
     val name: String
 )
--- app/src/main/java/com/aulakotlin/appnotas/data/model/Note.kt
+++ app/src/main/java/com/aulakotlin/appnotas/data/model/Note.kt
@@ -2,12 +2,25 @@ package com.aulakotlin.appnotas.data.model
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import androidx.room.ForeignKey
+import androidx.room.Index
 
-@Entity(tableName = "notes")
+@Entity(
+    tableName = "notes",
+    foreignKeys = [
+        ForeignKey(
+            entity = Folder::class,
+            parentColumns = ["id"],
+            childColumns = ["folderId"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ],
+    indices = [Index("folderId")]
+)
 data class Note (
-    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    @PrimaryKey(autoGenerate = true) val id: Long = 0,
     val title: String,
     val content: String,
     val timestamp: Long = System.currentTimeMillis(),
-    val folderId: Int
+    val folderId: Long
 )
